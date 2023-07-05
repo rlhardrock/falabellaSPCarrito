ea6a8740b391f7b8f9d0569cd826499afbde89a9
rlhardrock <rlhardrock@gmail.com>
2023-07-05 13:51:41 -0500

    reconfiguracion build.gradle

--- src/test/java/co/falabella/com/runners/CargarCarritoRunner.java
+++ src/test/java/co/falabella/com/runners/CargarCarritoRunner.java
@@ -1,2 +1,19 @@
-package co.falabella.com.runners;public class CargarCarritoRunner {
+package co.falabella.com.runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/cargarCarrito.feature",
+        tags = "@BuscarAgregarCarritoExito",
+        glue = "co.falabella.com.stepDefinitions",
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        plugin = "pretty",
+        monochrome= true
+)
+
+public class CargarCarritoRunner {
+
 }
